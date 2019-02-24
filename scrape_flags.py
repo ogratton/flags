45f2aa010b550ca3a04ed411e7a8f3eac93bce7a
Oliver Gratton <ojgratton@gmail.com>
2019-02-24 11:45:46 +0000

    Get high res files properly

--- scrape_flags.py
+++ scrape_flags.py
@@ -49,11 +49,28 @@ class WikiFlagScraper:
         # TODO On KeyError, try and fetch from wiki
         url = self.url_dict[country]
         if high_res:
-            # TODO they aren't all 23px
-            url = url.replace('23px', '1024px')
+            url = self.get_high_res_url(country)
 
         print(f"> Getting Flag of {country}: {url}")
         return self.get_image_from_url(url)
+
+    def get_high_res_url(self, country) -> str:
+        """
+        Fetch the larger version of the thumbnail
+        Unfortunately, we can't just go straight to "/File:Flag_of_{country}"
+        because (e.g.) Ireland doesn't follow the pattern. So we go via the
+        actual wiki pages and trust the wiki redirect system
+        """
+        wiki_stem = "https://en.wikipedia.org"
+        country_page = requests.get(f"{wiki_stem}/wiki/{country}")
+        country_doc = HTML(country_page.content)
+        [v_card] = country_doc.xpath('.//table[@class="infobox geography vcard"]')
+        [flag_elem] = v_card.xpath('.//a[@class="image" and contains(@title, "Flag")]')
+        flag_page_url = f"{wiki_stem}{flag_elem.attrib['href']}"
+        flag_page = requests.get(flag_page_url)
+        doc = HTML(flag_page.content)
+        [flag_url_elem] = doc.xpath('.//div[@id="file"]/a/img')
+        return f"https:{flag_url_elem.attrib['src']}"
 
     @classmethod
     @cached_to_file
